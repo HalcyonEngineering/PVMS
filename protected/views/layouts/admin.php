5e4ab90625a03bbbfec5d43ee0fbaefade40bec0
Jon Wou <jon.wou@hotmail.com>
2014-02-21 15:15:55 -0800

    Added Icon's to the navigation sidebar, Andy will edit them

--- protected/views/layouts/admin.php
+++ protected/views/layouts/admin.php
@@ -1,5 +1,9 @@
-
 <?php
+    $projectIcon= CHtml::image(Yii::app()->getBaseUrl().'/images/folder.png', "", array("width"=>"70px", "height"=>"50px"));
+    $volunteerIcon=CHtml::image(Yii::app()->getBaseUrl().'/images/wlm.png', "", array("width"=>"70px", "height"=>"50px"));
+    $calendarIcon=CHtml::image(Yii::app()->getBaseUrl().'/images/calendar.png', "", array("width"=>"70px", "height"=>"50px"));
+    $reportIcon=CHtml::image(Yii::app()->getBaseUrl().'/images/tasks.png', "", array("width"=>"70px", "height"=>"50px"));
+
     $this->widget(
                   'bootstrap.widgets.TbMenu',
                   array(
@@ -8,10 +12,10 @@
                         'items' => array(
                                          array('label'=>'Organizations', 'url'=>array('site/page', 'view'=>'projects')),
                                          array('label'=>'Managers', 'url'=>array('site/page', 'view'=>'import')),
-                                         array('label'=>'Calendar', 'url'=>array('site/page', 'view'=>'calendar')),
-                                         array('label'=>'Reports', 'url' =>array('organization/index')),
+                                         array('label'=>$calendarIcon, 'url'=>array('site/page', 'view'=>'calendar')),
+                                         array('label'=>$reportIcon, 'url' =>array('organization/index')),
                                          //array('label'=>'Email', 'url'=>array('mail/contact')),
                                          ),
                         ));
-    
+
     ?>
