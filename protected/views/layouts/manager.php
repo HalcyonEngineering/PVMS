72b9fdb6c4150b19192286d59fb52874a5253944
Jon Wou <jon.wou@hotmail.com>
2014-02-21 17:54:23 -0800

    Added submenus for ui

--- protected/views/layouts/manager.php
+++ protected/views/layouts/manager.php
@@ -12,8 +12,18 @@
                         'type' => 'tabs',
                         'stacked'=>true,
                         'items' => array(
-                                         array('label'=>$projectIcon, 'url'=>array('site/page', 'view'=>'projects')),
-                                         array('label'=>$volunteerIcon, 'url'=>array('site/page', 'view'=>'import')),
+                                         array('label'=>$projectIcon, 'url'=>array('site/page', 'view'=>'projects'),
+                                               'items'=> array(
+                                                               array('label'=>'List of Projects', 'url'=>'#'),
+                                                               array('label'=>'Project', 'url'=>'#'),
+                                                               )),
+                                         array('label'=>$volunteerIcon, 'url'=>array('site/page', 'view'=>'import'),
+                                               'items'=> array(
+                                                               array('label'=>'Add Individual', 'url'=>'csv/add_volunteers'),
+                                                               array('label'=>'Import Volunteers', 'url'=>'csv/import'),
+                                                               array('label'=>'Manage/Find', 'url'=>'#'),
+                                                               array('label'=>'Roles', 'url'=>'#'),
+                                                               array('label'=>'Tasks', 'url'=>'#'))),
                                          array('label'=>$calendarIcon, 'url'=>array('site/page', 'view'=>'calendar')),
                                          array('label'=>$reportIcon, 'url' =>array('organization/index')),
                                          //array('label'=>'Email', 'url'=>array('mail/contact')),
