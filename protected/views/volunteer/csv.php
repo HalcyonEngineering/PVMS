1fd7b80bc407ac4c35c5fa5f960bd8721e552099
SourceDave <david.d.li@gmail.com>
2014-03-04 17:08:22 -0800

    Fixed CUploadFile issue, thanks Kenneth!

--- protected/views/volunteer/csv.php
+++ protected/views/volunteer/csv.php
@@ -1,24 +1,22 @@
 <?php 
-    $form = $this->beginWidget(
-        'CActiveForm', 
-        array(
-            'id'=>'add-volunteer-manual-form',
-            'enableClientValidation'=>true,
-            'clientOptions'=>array('validateOnSubmit'=>true,),
-            'enableAjaxValidation'=>false,
-        )
-    ); 
-?>
 
-<h2>By CSV</h2>
+$form = $this->beginWidget(
+    'CActiveForm', 
+    array(
+        'id'=>'add-volunteer-manual-form',
+        'enableClientValidation'=>true,
+        'clientOptions'=>array('validateOnSubmit'=>true,),
+        'enableAjaxValidation'=>false,
+        'htmlOptions' => array('enctype' => 'multipart/form-data')
+    )
+); 
 
-<?php
+echo '<h2>By CSV</h2>';
 
 echo CHtml::link('Download file here<br><br>', $csvModel->getCsvTemplateUrl());
 echo CHtml::image($csvModel->getCsvTemplateImageUrl(), 'This is where the CSV template image would be...');
 
-echo '<br>';
-echo '<br>';
+echo '<br><br>';
 
 echo $form->fileField($csvModel, 'csv');
 echo $form->error($csvModel, 'csv');
