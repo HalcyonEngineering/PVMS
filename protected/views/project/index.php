781e2bbe50cc44028d4774f0afdc712f709b8a9c
KennethShen <Kenneth.Shen@alumni.ubc.ca>
2014-03-09 13:35:58 -0700

    Abstracted away some of modal logic.
    Controllers now have a modified redirect function that does a proper
    redirect on an ajax call.
    
    Controller and all subclasses now have a renderModal method, which
    generates a modal for ajax calls and a normal page for requests.
    
    Moved the modal to the main.php layout so it is accessible everywhere.

--- protected/views/project/index.php
+++ protected/views/project/index.php
@@ -11,8 +11,8 @@
 			                    'type'=>'POST',
 			                    // ajax post will use 'url' specified above
 			                    'url'=>"js:$(this).attr('href')",
-			                    'update'=>'#project-modal-body',
-			                    'complete'=>"$('#project-modal').modal('show')",
+			                    'update'=>'#modal-body',
+			                    'complete'=>"$('#modal').modal('show')",
 		                    ),
 	                    ),
                     ));
@@ -23,14 +23,3 @@
 'dataProvider'=>$dataProvider,
 'itemView'=>'_view',
 )); ?>
-
-<?php $this->beginWidget('bootstrap.widgets.TbModal', array(
-	'id'=>'project-modal',
-	'options'=>array(
-		'autoOpen'=>false, //important!
-		),
-	));
-?>
-<div id="project-modal-body" class="modal-body"></div>
-
-<?php $this->endWidget();?>
