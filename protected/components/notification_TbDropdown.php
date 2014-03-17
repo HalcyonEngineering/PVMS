cd918200079ba9c1e12c3d29fbcfac3c4995e282
KennethShen <Kenneth.Shen@alumni.ubc.ca>
2014-03-17 10:43:22 -0700

    Fixed a bug with the notifications.
    Notification dropdowns would fail to load when a user had a notification.

--- protected/components/notification_TbDropdown.php
+++ protected/components/notification_TbDropdown.php
@@ -14,13 +14,9 @@ class notification_TbDropdown extends TbMenu {
 
     public function init() {
         if ($this->visible) {
-            $notify_criteria = new CDbCriteria;
-            $notify_criteria->compare('user_id', Yii::app()->user->getId(), true); //TO-DO: Add Criteria to show unread only items
-            //The above section is a DBCriteria called "notify_criteria" used to select the notification entities with criteria of having the currently login ID.
-            $Notification_dataprovider = new CActiveDataProvider('Notification', array('criteria' => $notify_criteria,));
             $notificationIcon = CHtml::image(Yii::app()->getBaseUrl() . '/images/Notificationbutton.png', 'notification', array('class' => 'img-circle', 'id' => 'notification-icon'));
             ob_start();
-            $this->widget('bootstrap.widgets.TbListView', array('dataProvider' => $Notification_dataprovider, 'itemView' => '/notification/_notification'));
+            $this->renderNotifications();
             $nWidget = ob_get_clean();
             //$this->count = $Notification_dataprovider->itemCount;
             //This is to get the count of notifications CURRENTLY ON THE PAGE. Use totalItemCount to return the total number of data items.
@@ -47,6 +43,17 @@ class notification_TbDropdown extends TbMenu {
         }
     }
 
+	public function renderNotifications(){
+		$notify_criteria = new CDbCriteria;
+        $notify_criteria->compare('user_id', Yii::app()->user->id, true); //TO-DO: Add Criteria to show unread only items
+        //The above section is a DBCriteria called "notify_criteria" used to select the notification entities with criteria of having the currently login ID.
+        $Notification_dataprovider = new CActiveDataProvider('Notification', array('criteria' => $notify_criteria,));
+		$this->widget('bootstrap.widgets.TbListView',
+					  array('dataProvider' => $Notification_dataprovider, 
+							'itemView' => '/_notification')
+					  );
+	}
+	
     public function run() {
         parent::run();
 
