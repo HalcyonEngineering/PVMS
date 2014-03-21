8ebe344c3b123702809937ee4cb679465c000bce
Jon Wou <jon.wou@hotmail.com>
2014-03-20 18:20:27 -0700

    Changed the layout, and added more testing data

--- protected/data/schema.sqlite.sql
+++ protected/data/schema.sqlite.sql
@@ -156,7 +156,7 @@ INSERT INTO pvms_lookup (name, type, code, position) VALUES ('Complete (Verified
 INSERT INTO pvms_user (name, password, email, type) VALUES ('demo','$2a$10$JTJf6/XqC94rrOtzuF397OHa4mbmZrVTBOQCmYD9U.obZRUut4BoC','webmaster@example.com', 0);
 INSERT INTO pvms_user (name, password, email, type) VALUES ('admin','$2a$10$xOHcdC9nHnzQeOYtw3jwUu1Nc87gDo9P9YGQYWLVQNMxJEZqZiL2y','admin', 0);
 --pw for Sean is manager
-INSERT INTO pvms_user (name, password, email, type) VALUES ('Sean','$2a$12$asAXUgsB3jixPd7PA5qrBe1ptevmxrl3eb8J8VuIMJSRVYZok1V/m','sean@pitchn.ca', 1);
+INSERT INTO pvms_user (name, password, email, type) VALUES ('Sean Kennedy','$2a$12$asAXUgsB3jixPd7PA5qrBe1ptevmxrl3eb8J8VuIMJSRVYZok1V/m','sean@pitchn.ca', 1);
 INSERT INTO pvms_user (name, password, email, type) VALUES ('manager2','$2a$12$MO94I98I9ts.psar1DoMBOxfKAZXciOPDwqUF4UJ8P5hua.my5EPO','manager2', 1);
 INSERT INTO pvms_user (name, password, email, type) VALUES ('volunteer','$2a$12$J1n3OwZasqX3gsMG6TSzvOHEJleCYyWJ/TNAuxOmAoB/zmiBqskeq','volunteer', 2);
 INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('6','David','$2a$13$dEoNvuwcQ/7xRSebI6tO6edjUTXKfElOjnZz7TK6ymhT3Z.cNb.0G','david@pitchn.ca','Surrey','Project Management, Web Development, Software Engineering',NULL,'2','0',NULL);
@@ -166,20 +166,24 @@ INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","c
 INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('10','Andy','$2a$13$SRvsTXfWXGDuLtYjDZlcVOVVa2Wjv.eXeHYQQXSwqEKMiNcvuUc9C','andy@pitchn.ca','Whiterock','Software Engineering',NULL,'2','0',NULL);
 -- 10 Users Above
 INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('11','Phat','$2a$13$TDoAQfWsoD.9t54gTysg7efNlO4.8P07GDewLiigpjWG16Y9.XOt2','phat@pitchn.ca','Vancouver','Software Engineering, Parkour',NULL,'2','0',NULL);
-INSERT INTO pvms_user (name, password, email, type) VALUES ('Tyrone Black', '$2a$12$UCNKdKUrGLDCeqeBfwxZQ.VQ1ZqGwXl59niTxQQJEXpsor9MZzZrq', 'tyrone.black@gmail.com', 2);
---pw for Tristan is tristan
+INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('12','Deepak','$2a$13$TDoAQfWsoD.9t54gTysg7efNlO4.8P07GDewLiigpjWG16Y9.XOt2','dazad@gmail.com','Vancouver','Education',NULL,'2','0',NULL);
+--pw for Jenny is jenny
 INSERT INTO pvms_user (name, password, email, type) VALUES ('Jenny', '$2a$12$BN7dwapvVqvDaBlRBQPYneNUFkMwdjhlRFNwrtcNKs80zuKt5vNvq', 'jenny@rec.ubc.ca', 1);
 --pw for Jonathan is password
-INSERT INTO pvms_user (name, password, email, type) VALUES ('Jonathan', '$2a$12$L7Bt02zTY.35HCHODI3IDecUTLWG.iEEC1njyIBqJf/ZJXPgbbMmC', 'jonathan@rec.ubc.ca', 1);
-INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('15','Harry','$2a$13$SRvsTXfWXGDuLtYjDZlcVOVVa2Wjv.eXeHYQQXSwqEKMiNcvuUc9C','harry@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
-INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('16','Billy','$2a$13$SRvsTXfWXGDuLtYjDZlcVOVVa2Wjv.eXeHYQQXSwqEKMiNcvuUc9C','billy@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
-INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('17','Rex','$2a$13$SRvsTXfWXGDuLtYjDZlcVOVVa2Wjv.eXeHYQQXSwqEKMiNcvuUc9C','rex@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
-INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('18','Joel','$2a$13$SRvsTXfWXGDuLtYjDZlcVOVVa2Wjv.eXeHYQQXSwqEKMiNcvuUc9C','joel@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
-INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('19','Melissa','$2a$13$SRvsTXfWXGDuLtYjDZlcVOVVa2Wjv.eXeHYQQXSwqEKMiNcvuUc9C','melissa@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
-INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('20','Gord','$2a$13$SRvsTXfWXGDuLtYjDZlcVOVVa2Wjv.eXeHYQQXSwqEKMiNcvuUc9C','gord@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
-INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('21','Lucas','$2a$13$SRvsTXfWXGDuLtYjDZlcVOVVa2Wjv.eXeHYQQXSwqEKMiNcvuUc9C','lucas@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
-INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('22','Emma','$2a$13$SRvsTXfWXGDuLtYjDZlcVOVVa2Wjv.eXeHYQQXSwqEKMiNcvuUc9C','emma@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
-INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('23','Olivia','$2a$13$SRvsTXfWXGDuLtYjDZlcVOVVa2Wjv.eXeHYQQXSwqEKMiNcvuUc9C','olivia@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
+INSERT INTO pvms_user (name, password, email, type) VALUES ('Jonathan Wou', '$2a$12$L7Bt02zTY.35HCHODI3IDecUTLWG.iEEC1njyIBqJf/ZJXPgbbMmC', 'jonathan@rec.ubc.ca', 1);
+
+--Volunteers start
+-- pw for Volunteers is password
+INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('15','Harry','$2a$12$L7Bt02zTY.35HCHODI3IDecUTLWG.iEEC1njyIBqJf/ZJXPgbbMmC','harry@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
+INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('16','Billy','$2a$12$L7Bt02zTY.35HCHODI3IDecUTLWG.iEEC1njyIBqJf/ZJXPgbbMmC','billy@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
+INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('17','Rex','$2a$12$L7Bt02zTY.35HCHODI3IDecUTLWG.iEEC1njyIBqJf/ZJXPgbbMmC','rex@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
+INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('18','Joel','$2a$12$L7Bt02zTY.35HCHODI3IDecUTLWG.iEEC1njyIBqJf/ZJXPgbbMmC','joel@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
+INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('19','Melissa','$2a$12$L7Bt02zTY.35HCHODI3IDecUTLWG.iEEC1njyIBqJf/ZJXPgbbMmC','melissa@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
+INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('20','Gord','$2a$12$L7Bt02zTY.35HCHODI3IDecUTLWG.iEEC1njyIBqJf/ZJXPgbbMmC','gord@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
+INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('21','Lucas','$2a$12$L7Bt02zTY.35HCHODI3IDecUTLWG.iEEC1njyIBqJf/ZJXPgbbMmC','lucas@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
+INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('22','Emma','$2a$12$L7Bt02zTY.35HCHODI3IDecUTLWG.iEEC1njyIBqJf/ZJXPgbbMmC','emma@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
+INSERT INTO "pvms_user" ("id","name","password","email","location","skillset","causes","type","adminAccess","profile") VALUES ('23','Olivia','$2a$12$L7Bt02zTY.35HCHODI3IDecUTLWG.iEEC1njyIBqJf/ZJXPgbbMmC','olivia@rec.ubc.ca','Vancouver','School',NULL,'2','0',NULL);
+-- Volunteers end
 
 -- Organizations
 INSERT INTO pvms_organization (name, desc) VALUES ('Pitchn', 'This is Pitchn Solutions.');
@@ -196,16 +200,16 @@ INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (1,'Promotions', 'A 
 INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (1,'Networking', 'Creating new connections in the Startup Company.','#333333');
 INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (1,'Misc', 'Random roles and tasks to be completed','#444444');
 INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (2,'First Project', 'First project created','#444444');
-INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Soccer', 'Handley Cup Soccer League is great','#FFFFFF');
-INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Futsal', 'SRC Futsal league still needs a name','#FFFFFF');
-INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Ball Hockey', 'Bodin Ball Hockey league','#FFFFFF');
-INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Ice Hockey', 'Todd Ice Hockey league is great','#FFFFFF');
-INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Basketball', 'Nitobe Basketball league','#FFFFFF');
-INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Volleyball', 'Cross Volleyball has multiple leagues.','#FFFFFF');
-INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Dodgeball', 'The is an amazing league to play.','#FFFFFF');
-INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Football', 'The Point Grey football league runs Sunday nights.','#FFFFFF');
-INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Ultimate', 'The Ultimate league is not the ultimate league.','#FFFFFF');
-INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Storm the Wall', 'Storm the wall is March 23-25th.','#FFFFFF');
+INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Soccer', 'Handley Cup Soccer League is great','#aaaaaa');
+INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Futsal', 'SRC Futsal league still needs a name','#bbbbbb');
+INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Ball Hockey', 'Bodin Ball Hockey league','#ababab');
+INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Ice Hockey', 'Todd Ice Hockey league is great','#cdcdcd');
+INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Basketball', 'Nitobe Basketball league','#dddddd');
+INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Volleyball', 'Cross Volleyball has multiple leagues.','#121212');
+INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Dodgeball', 'The is an amazing league to play.','#efefef');
+INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Football', 'The Point Grey football league runs Sunday nights.','#000000');
+INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Ultimate', 'The Ultimate league is not the ultimate league.','#22222');
+INSERT INTO pvms_project(org_id, name, desc, colour) VALUES (3,'Storm the Wall', 'Storm the wall is March 23-25th.','#2e2e2e');
 
 --Roles
 INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (1, 'Web Developer', 'First.', '#FFFFFF');
@@ -213,22 +217,28 @@ INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (1, 'PHP Programmer
 INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (1, 'Marketing Assistant', 'First.', '#111111');
 INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (1, 'Sponsorship Assistant', 'First.', '#222222');
 INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (2, 'First Role', 'First role created.', '#FFFFFF');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' Route Patrol', 'First role created.', '#FFFFFF');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' Wall Spotter', 'First role created.', '#000000');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' DJ && MC', 'First role created.', '#111111');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' Cycle Transition', 'First role created.', '#222222');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' Pool Transition', 'First role created.', '#333333');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' Communications Assistant Director', 'First role created.', '#444444');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' Media Assistant Director', 'First.', '#555555');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' Eligibility Assistant Director', 'First.', '#666666');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, '  Scheduling Assistant Director', 'First.', '#777777');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' Social Assistant Director', 'First.', '#8888888');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' Referee Development Assistant Director', 'First .', '#aaaaaa');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' Referee CoordinatorAssistant Director', 'First .', '#bbbbbb');
-INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, ' Community Assistant Director', 'First.', '#cccccc');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Route Patrol', 'First role created.', '#FFFFFF');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Wall Spotter', 'First role created.', '#000000');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'DJ && MC', 'First role created.', '#111111');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Cycle Transition', 'First role created.', '#222222');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Pool Transition', 'First role created.', '#333333');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Communications Assistant Director', 'First role created.', '#444444');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Media Assistant Director', 'First.', '#555555');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Eligibility Assistant Director', 'First.', '#666666');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Scheduling Assistant Director', 'First.', '#777777');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Social Assistant Director', 'First.', '#8888888');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Referee Development Assistant Director', 'First .', '#aaaaaa');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Referee CoordinatorAssistant Director', 'First .', '#bbbbbb');
+INSERT INTO pvms_role(project_id, name, desc, colour) VALUES (3, 'Community Assistant Director', 'First.', '#cccccc');
 
 -- Tasks
+INSERT INTO pvms_task(role_id, name, desc) VALUES(1, 'Create new UI', 'This task needs to be completed.');
+INSERT INTO pvms_task(role_id, name, desc) VALUES(1, 'Get Sponsorship', 'Contact smaller companies to get earn sponsorships.');
+INSERT INTO pvms_task(role_id, name, desc) VALUES(1, 'Update Database', 'Add more volunteers.');
+INSERT INTO pvms_task(role_id, name, desc) VALUES(1, 'Meet with Jason', 'Meeting is at Pitchn HQ in Downtown Vancouver on Monday at noon.');
+INSERT INTO pvms_task(role_id, name, desc) VALUES(1, 'Hand in documents', 'This task needs to be completed.');
 INSERT INTO pvms_task(role_id, name, desc) VALUES(2, 'First Task', 'First task created.');
+
 
 INSERT INTO pvms_onboarding(role_id, onboarding_welcome, onboarding_instructions, onboarding_contact) VALUES(1,'Welcome!', 'get work done', 'kimjongun@wpk.kp');
 INSERT INTO pvms_file(project_id, file_name, file_size, file_data) VALUES(1,'example.txt',19,'This is an example.');
