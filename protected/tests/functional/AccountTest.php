18e72f635c1a708e14c756b0606944793fbb9e02
Phattrick Tran <phattrick.tran@gmail.com>
2014-03-23 02:59:33 -0700

    Modified selenium config and added sample login test

--- protected/tests/functional/AccountTest.php
+++ protected/tests/functional/AccountTest.php
@@ -10,12 +10,15 @@ class AccountTest extends WebTestCase
 			$this->clickAndWait('link=Logout');
 
 		// test login process, including validation
+		$this->pause(2000);
 		$this->clickAndWait('link=Login');
 		$this->assertElementPresent('name=LoginForm[username]');
-		$this->type('name=LoginForm[username]','demo');
+		$this->type('name=LoginForm[username]','admin');
+		$this->pause(2000);
 		$this->clickAndWait("//input[@value='Login']");
 		$this->assertTextPresent('Password cannot be blank.');
-		$this->type('name=LoginForm[password]','demo');
+		$this->type('name=LoginForm[password]','admin');
+		$this->pause(2000);
 		$this->clickAndWait("//input[@value='Login']");
 		$this->assertTextNotPresent('Password cannot be blank.');
 		$this->assertTextPresent('Logout');
@@ -25,4 +28,5 @@ class AccountTest extends WebTestCase
 		$this->clickAndWait('link=Logout');
 		$this->assertTextPresent('Login');
 	}
+}
 ?>
