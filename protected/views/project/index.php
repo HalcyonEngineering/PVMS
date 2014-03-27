071d48aa477fddd178395db54990866b56e0041b
Jon Wou <jon.wou@hotmail.com>
2014-03-27 16:10:50 -0700

    Made welcome page nicer

--- protected/views/project/index.php
+++ protected/views/project/index.php
@@ -1,9 +1,15 @@
-<?php
+<center><?php
     $user = Yii::app()->user->name;
-    echo "<h1>Welcome, ". $user ."</h1>";
+    echo "<h1>Hello, ". $user ."</h1>";
     ?>
-<p>Here are your projects. You can also create a new project by clicking on the button below</p></br>
-
+<div style="width: 75%;">
+<p> Pitch'n is is here to assist you manage your projects and volunteers. You can organize your volunteers using the Tabs on the side. If you have any questions please see the FAQ in the top right corner.</p> </div>
+</center>
+<hr> </hr>
+<center>
+<h2> Projects </h2>
+<p> Here are your projects. You can also create a new project by clicking on the button below</p></br>
+</center>
 <?php $this->widget('ModalOpenButton',
                     array(
 	                    'button_id'=>'create-project-btn',
