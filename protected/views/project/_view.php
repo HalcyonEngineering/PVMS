68b8f1eb83443d456a08af9b21553dac7a08b711
Jon Wou <jon.wou@hotmail.com>
2014-03-28 11:48:25 -0700

    Added edit button and changed the partical view of projects

--- protected/views/project/_view.php
+++ protected/views/project/_view.php
@@ -1,6 +1,9 @@
 <div class="tile-view span-6">
 	<div class="tile" style=<?php echo "border-color:" . CHtml::encode($data->colour).";";?>>
-		<h3><b><?php echo CHtml::link(CHtml::encode($data->name),array('/project/view','id'=>$data->id)); ?></b></h3>
+		<h3><b><?php echo CHtml::encode($data->name); ?></b>
+
+</h3>
+
         <div class="tile-top">
 	    <?php echo CHtml::encode($data->desc); ?>
 	    <br />  <br />
@@ -21,16 +24,31 @@
 
 		$this->renderPartial('/task/_progressBar',array('data'=>$data));
 		?>
+<?php
+$this->widget(
+              'bootstrap.widgets.TbButton',
+              array(
+                    'label' => 'View',
+                    'type' => 'primary',
+                    'url' => array('/project/view','id'=>$data->id),
+                    )
+              );
+    ?>
 
-		<?php
-		$this->widget('ModalOpenButton',
-		              array('label' => 'Edit Project',
-		                    'type' => 'link',
-		                    'button_id'=>'edit-project-'.$data->id,
-		                    'url' => Yii::app()->createUrl("project/update", array("id"=>$data->id))
-		              )
-		);
-		?>
+<?php
+        Yii::import('bootstrap.helpers.TbHtml');
+        $this->widget('ModalOpenButton',
+                      array('label' => TbHtml::icon(TbHtml::ICON_PENCIL),
+                            'type' => 'link',
+                            'encodeLabel' =>false,
+                            'button_id'=>'edit-project-'.$data->id,
+                            'url' => Yii::app()->createUrl("project/update", array("id"=>$data->id))
+                            )
+                      );
+        ?>
+
+
+
       </div>
 
 	</div>
