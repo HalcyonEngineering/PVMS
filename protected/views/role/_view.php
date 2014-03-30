b7fe33b2770292d15cbf139845afe828cfd5ab3d
Jon Wou <jon.wou@hotmail.com>
2014-03-29 17:18:39 -0700

    Added an icon for delete role from Volunteer page

--- protected/views/role/_view.php
+++ protected/views/role/_view.php
@@ -1,44 +1,42 @@
 <div class="tile-view span-6">
 	<div class="tile" style=<?php echo "border-color:" . CHtml::encode($data->colour).";";?>>
 
-<h3> <b><?php echo CHtml::link(CHtml::encode($data->name),array('/role/view','id'=>$data->id)); ?></b></h3>
-    <br />
+<h3 style="margin: 0px;"> <b><?php echo CHtml::link(CHtml::encode($data->name),array('/role/view','id'=>$data->id)); ?></b></h3>
 
+<font size=1>
+<?php echo CHtml::encode($data->project->name); ?>
+</font>
+<br /><br />
 
 
 	<?php echo CHtml::encode($data->desc); ?>
 	<br /><br />
 
-    <b><?php echo CHtml::encode('Project'); ?>:</b>
-    <?php echo CHtml::encode($data->project->name); ?>
-    <br /><br />
 
 <b><?php echo CHtml::encode('Project Description'); ?>:</b>
 <?php echo CHtml::encode($data->project->desc); ?>
 <br /><br />
 
-
+<div class="tile-bottom">
 	<b><?php echo CHtml::encode('Number of Volunteers'); ?>:</b>
 	<?php echo CHtml::encode(count($data->users)); ?>
-	<br />
+	<br /><br />
 	<?php
 		$this->renderPartial('/task/_progressBar',array('data'=>$data));
 	?>
 
-
-
 <?php
-    /* Trying to workout opt out functionality
     Yii::import('bootstrap.helpers.TbHtml');
     $this->widget('bootstrap.widgets.TbButton',
                   array('label' => TbHtml::icon(TbHtml::ICON_TRASH),
                         'type' => 'link',
+                        'buttonType' => 'sumbit',
                         'encodeLabel' =>false,
                         'url'=>'Yii::app()->createUrl("volunteer/remove",array("id"=>$data->id))',
                         )
-                  );*/
+                  );
     ?>
 
 
-</div>
+</div></div>
 </div>
