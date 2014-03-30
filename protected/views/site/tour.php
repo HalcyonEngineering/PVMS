cc9048b8cba4d272670424746b5ad8f91242f7c8
Jon Wou <jon.wou@hotmail.com>
2014-03-30 14:57:34 -0700

    Check out the Take a Tour Videos

--- protected/views/site/tour.php
+++ protected/views/site/tour.php
@@ -1,5 +1,14 @@
 
 <div class="span-19 text-center">
 <h1>Tour</h1>
-<iframe width="560" height="315" src="//www.youtube.com/embed/vYyESg-Iqh4" frameborder="0" allowfullscreen></iframe>
+<?php if (Yii::app()->user->isAdmin()){
+    //Admin Video
+  echo  "<iframe width=\"560\" height=\"315\" src=\"//www.youtube.com/embed/wLKtTTZrq7k\" frameborder=\"0\" allowfullscreen></iframe>";
+}else if (Yii::app()->user->isManager()){
+       //Manager Video
+   echo "<iframe width=\"560\" height=\"315\" src=\"//www.youtube.com/embed/vYyESg-Iqh4\" frameborder=\"0\" allowfullscreen></iframe>";
+}else{
+    //Volunteer Video
+echo "<iframe width=\"560\" height=\"315\" src=\"//www.youtube.com/embed/wjivo98WIIA\" frameborder=\"0\" allowfullscreen></iframe>";
+}?>
 </div>
