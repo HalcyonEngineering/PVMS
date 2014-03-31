b49c347d036707e132ba7e71cd5c641accff55af
Jon Wou <jon.wou@hotmail.com>
2014-03-30 19:57:20 -0700

    Formatted messages

--- protected/views/message/_view.php
+++ protected/views/message/_view.php
@@ -1,9 +1,15 @@
 <?php
 	$this->beginWidget('CHtmlPurifier');
 	echo '<div class="message-subject">';
-	echo "Subject :" . nl2br($data->subject);
+    echo '<div class="span-4">';
+	echo "<b>Subject : </b>" . nl2br($data->subject);
+    echo '</div>';
+    echo '<div class="span-4">';
+    echo "<b>Sent at: </b>".$data->timestamp;
+    echo '</div>';
+    echo '<br\>';
 	echo '</div>';
-	echo '<div class="message-body">';
+	echo '<div class="message-body span-18">';
 	echo nl2br($data->body);
 	echo '</div>';
 	$this->endWidget();
