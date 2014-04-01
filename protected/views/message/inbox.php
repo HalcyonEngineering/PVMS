4469a35ca2a3c6128f44aab99d96e4c52afd9a77
Andy Wang <andy_wang347@hotmail.com>
2014-04-01 12:10:20 -0700

    inbox button css

--- protected/views/message/inbox.php
+++ protected/views/message/inbox.php
@@ -1,31 +1,32 @@
 <?php
     
-    echo "<div class=\"span-4 pull-right\">";
+    echo "<div class=\"span-4 pull-right\" id=\"outbox-button-div\">";
     $this->widget(
                   'bootstrap.widgets.TbButton',
                   array(
                         'label' => 'Outbox',
                         'type' => 'Common',
+                        'id' => 'out-box-btn',
                         'url' => array('/message/outbox'),
                         )
                   );
     echo "</div>";
     
-    echo "<div class=\"span-4 pull-right\">";
-    $this->widget('ModalOpenButton',
-                  array(
-                        'label' => 'Send Message',
-                        'type' => 'Common',
-                        'encodeLabel' =>false,
-                        'button_id'=>'1',
-                        'url' => Yii::app()->createUrl("message/compose"),
-                        )
-                  );
-    echo "</div>";
+
+
+
    
     
 	echo CHtml::tag('h1', array(), 'Inbox');
-    
+$this->widget('ModalOpenButton',
+    array(
+        'label' => 'Send Message',
+        'type' => 'Common',
+        'encodeLabel' =>false,
+        'button_id'=>'1',
+        'url' => Yii::app()->createUrl("message/compose"),
+    )
+);
     
     
 	$columns=array(
