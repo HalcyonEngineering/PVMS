b940d59c67e0041f30694e3c37aab63756ccda03
SourceDave <david.d.li@gmail.com>
2014-04-04 19:02:51 -0700

    Modifed fixture for task to correspond to existing test cases

--- protected/tests/fixtures/pvms_task.php
+++ protected/tests/fixtures/pvms_task.php
@@ -14,21 +14,12 @@ return array(
 	// We can set aliases to database rows which makes it easier to call specific entries
 	// in our test
 	'sampleTask'=>array(
-		"id"=>'1',
-		"role_id"=>'1',
-		"name"=>'Sample Task Name',
-		"desc"=>null,
-		"expected"=>null,
-		"actual"=>null,
-		"status"=>'1',
-	),
-	'sampleTask2'=>array(
-		"id"=>'2',
-		"role_id"=>'2',
-		"name"=>'Sample Task 2 Name',
-		"desc"=>null,
-		"expected"=>null,
-		"actual"=>null,
-		"status"=>'1',
+		'id'=>'1',
+		'role_id'=>'10',
+		'name'=>'sampleTaskName',
+		'desc'=>'sampleTaskDesc',
+		'expected'=>null,
+		'actual'=>null,
+		'status'=>Task::STATUS_IN_PROGRESS,
 	),
 );
