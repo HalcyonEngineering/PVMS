4c4fcfc232150db93dfdaa51f62eae01a1584127
SourceDave <david.d.li@gmail.com>
2014-04-04 18:04:25 -0700

    Added CRUD unit tests for organization.

--- protected/tests/unit/OrganizationTest.php
+++ protected/tests/unit/OrganizationTest.php
@@ -2,21 +2,53 @@
 
 class OrganizationTest extends CDbTestCase
 {
-	public $fixtures=array();
+	public $fixtures=array(
+            'organizations'=>'Organzations',
+        );
 	
-	public function testDb()
-	{
-		$model = Organization::model()->findByPk(1);
-		$this->assertEquals($model->name, "Pitchn");
-	}
-	
-	public function testManagerDeletion()
-	{
-		$model = Organization::model()->findByPk(1);
-		$this->assertEquals($model->name, "Pitchn");
-		$manageremail = $model->getManager()->email;
-		$model->delete();
-		$this->assertEquals(User::model()->findByAttributes(array('email'=>'sean@pitchn.ca')), null);
-		
-	}
+        public function testCreateOrg()
+        {
+            $model = new Organzation();
+            $model->setAttributes(array(
+                'name'=>'Created organization',
+                'desc'=>'Created organization description',
+            ));
+        
+            $this->assertTrue($model->save());
+        }
+
+        public function testCreateNullOrg()
+        {
+            $model = new Organzation();
+            $this->assertFalse($model->save());
+        }
+
+        public function testReadOrg()
+        {
+            $org = $this->organizations('sampleOrg');
+            $this->assertEquals($org->id, '1');
+            $this->assertEquals($org->name, 'sampleOrgName');
+            $this->assertEquals($org->desc, 'sampleOrgDesc');
+        }
+
+        public function testUpdateOrg()
+        {
+            $org = $this->organizations('sampleOrg');
+            $org->setAttributes(array(
+                'name'=>'sampleOrgNameUpdated',
+                'desc'=>'sampleOrgDescUpdated',
+            ));
+            $this->assertTrue($org->save());
+            $this->assertEquals($org->id, '1');
+            $this->assertEquals($org->name, 'sampleOrgNameUpdated');
+            $this->assertEquals($org->desc, 'sampleOrgDescUpdated');
+        }
+
+        public function testDeleteOrg()
+        {
+            $org = $this->organizations('sampleOrg');
+            $this->assertTrue($org->delete);
+            $this->assertEquals(empty(Organization::models()->findAll()), true);
+        }
+            
 }
